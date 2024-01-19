4db1d7122630078a6e4209cc639185ad8611120b
Mateusz Borowski <m.borowski128@gmail.com>
2024-01-19 22:25:17 +0100

    Fix

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-OpenAI_API_Key = "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"
+OpenAI_API_Key = "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"
 Telegram_Bot_Key = "6391210300:AAGXKNVUbI8Ow2QtJCAlgRb3RkfeWOn3380"
 assistant_id = "asst_556gPLX8NXAjq27N5twckhjT"
